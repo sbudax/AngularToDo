d4da58795578e95ad4c0c71f372dd24d644205a4
DA M1 40 <sbusiso.xaba@dadevs.co.za>
2022-07-25 09:46:28 +0200

    deleted empty files

--- backend/src/app/routes/user.js
+++ backend/src/app/routes/user.js
@@ -69,7 +69,7 @@ check("password", "Please enter a valid password").isLength({ min: 6 })
 );
 
 
-router.post("/login", [
+router.post("/signin", [
     check("email", "Please enter a valid email").isEmail(),
     check("password", "Please enter a valid password").isLength({
         min: 6
